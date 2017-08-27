856554c60ebff16d0968ecdce4f2d32524a99626
Michal Stanke <michal@stanke.cz>
2017-08-27 13:40:38 +0200

    Options localization, fix #5

--- src/js/options-ui.js
+++ src/js/options-ui.js
@@ -3,6 +3,10 @@
 var options = new Options();
 var inputs = document.querySelectorAll('input');
 
+for (element of document.querySelectorAll('[data-l10n-id]')) {
+    element.textContent = chrome.i18n.getMessage(element.dataset.l10nId);
+}
+
 inputs.forEach(options.loadDefault.bind(options));
 options.loadAllFromLocalStorage();
 for (input of inputs) {
