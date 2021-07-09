4ab51377b2619cc8da534cce87c245b38ac78b56
azecevic <azecevic@startgroup.com.au>
2021-07-09 18:57:44 +1000

    Add .vs hidden folder to .gitignore. Add some comments and rearrange statements in content-script.js. First commit :)

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -1,23 +1,25 @@
 'use strict';
 
+// Initialize components.
 const options = new Options();
 const imageContainer = new ImageContainer(options);
-
-document.addEventListener('contextmenu', (e) => {
-    if (e.target.tagName.toLowerCase() === 'img') {
-        imageContainer.setImage(e.target);
-    } else {
-        const img = e.target.querySelector('img');
-        if (img) {
-            imageContainer.setImage(img);
-        } else {
-            imageContainer.setImage(null);
-        }
-    }
-}, true);
-
 const messageListener = new MessageListener(imageContainer);
 
+// Set image on context menu.
+document.addEventListener('contextmenu', (e) => {
+	if (e.target.tagName.toLowerCase() === 'img') {
+		imageContainer.setImage(e.target);
+	} else {
+		const img = e.target.querySelector('img');
+		if (img) {
+			imageContainer.setImage(img);
+		} else {
+			imageContainer.setImage(null);
+		}
+	}
+}, true);
+
+// Set up messaging between content and background scripts.
 browser.runtime.onMessage.addListener((request, sender, sendResponse) =>
-    messageListener.receive(request, sender, sendResponse)
+	messageListener.receive(request, sender, sendResponse)
 );
